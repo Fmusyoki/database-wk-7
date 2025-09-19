38b1aae22d08f31aae152647792b05b7014ad471
Fred Musyoki <72469298+Fmusyoki@users.noreply.github.com>
2025-09-19 17:58:56 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,30 @@
+/*QUESTION 1*/
+SELECT 
+    OrderID,
+    CustomerName,
+    TRIM(value) AS Product
+FROM ProductDetail
+CROSS APPLY STRING_SPLIT(Products, ',');
+
+
+/*QUESTION 2*/
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
+
